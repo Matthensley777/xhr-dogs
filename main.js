82c5d54a8bc9a0ba0e1a1426cbe0414e27a2fd8e
Matt Hensley <matt.hensley777@gmail.com>
2017-08-28 19:09:51 -0500

    more stuff

--- main.js
+++ main.js
@@ -2,27 +2,36 @@ var dogs = [];
 
 var dogContainer = document.getElementById("dogContainer");
 
-function dogBreeds(dogs) {
+function dogList(dogs) {
     var dogBuilder = "";
+    var currentDog;
     for (var i = 0; i < dogs.length; i++) {
+        currentDog = dogs[i];
         dogBuilder += `<div class="dogs">`;
-        dogBuilder += `<h3>${dogs.name}</h3>`;
-        dogBuilder += `<p>${dogs.color}</p>`
-        dogBuilder += `<p>${dogs.add-on-price}</p>`
+        dogBuilder += `<h3>${dogs[i].name}</h3>`;
+        dogBuilder += `<p>${dogs[i].color}</p>`
         dogBuilder += `</div>`
     }
     dogContainer.innerHTML = dogBuilder;
 };
 
+
+
 function useForDogs() {
 
     var dogs = JSON.parse(this.responseText).dogs;
     console.log("dogs", dogs)
-    dogBreeds();
+    dogList(dogs);
 }
 
 function shitsBroke() {
     console.log("what did you do?")
+}
+
+function useForBreeds() {
+
+    var breeds = JSON.parse(this.responseText).breeds;
+    console.log("breeds", breeds)
 }
 
 var myRequest = new XMLHttpRequest();
@@ -30,4 +39,8 @@ myRequest.addEventListener("load", useForDogs);
 myRequest.open("GET", "dogs.json");
 myRequest.send();
 
-console.log(myRequest)
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", useForBreeds);
+myRequest2.open("GET", "breeds.json");
+myRequest2.send();
+
